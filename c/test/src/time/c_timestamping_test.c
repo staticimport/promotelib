5b911af258707f84ba05b08ad744e57078a76eab
Craig Bowles <cbowles@archbox.(none)>
2011-09-08 20:41:33 -0500

    Added gettimeofday and rdtscp tests

--- c/test/src/time/c_timestamping_test.c
+++ c/test/src/time/c_timestamping_test.c
@@ -1,13 +1,16 @@
 
 #include <stdint.h>
+#include <stdio.h>
+#include <sys/time.h>
 #include <time.h>
 
 #include "stopwatch.h"
 #include "testing.h"
 
 static uint64_t perf_test_clock_gettime(clockid_t id);
-//static uint64_t perf_test_rdtsc();
-//static uint64_t rdtsc();
+static uint64_t perf_test_gettimeofday();
+static uint64_t perf_test_rdtsc();
+static inline uint64_t my_rdtsc();
 
 int main(void)
 {
@@ -23,7 +26,8 @@ int main(void)
                         perf_test_clock_gettime(CLOCK_PROCESS_CPUTIME_ID));
   pro_print_perf_result("clock_gettime(CLOCK_THREAD_CPUTIME_ID)",
                         perf_test_clock_gettime(CLOCK_THREAD_CPUTIME_ID));
-  //pro_print_perf_result("rdtsc", perf_test_rdtsc());
+  pro_print_perf_result("gettimeofday", perf_test_gettimeofday());
+  pro_print_perf_result("rdtsc", perf_test_rdtsc());
   return 0;
 }
 
@@ -41,24 +45,50 @@ static uint64_t perf_test_clock_gettime(clockid_t id)
   return pro_stopwatch_elapsed_nanos(&stopwatch) / iters;
 }
 
-/*static uint64_t perf_test_rdtsc()
+static uint64_t perf_test_gettimeofday()
 {
+  struct timeval tv;
   pro_stopwatch_t stopwatch;
   uint64_t const iters = 1000000;
   pro_stopwatch_init(&stopwatch);
   pro_stopwatch_start(&stopwatch);
   for(uint64_t ii = 0; ii != iters; ++ii) {
-    rdtsc();
+    gettimeofday(&tv, 0);
   }
   pro_stopwatch_stop(&stopwatch);
   return pro_stopwatch_elapsed_nanos(&stopwatch) / iters;
-}*/
+}
 
-//static uint64_t rdtsc()
-//{
-//  unsigned a,d;
-//  //asm("cpuid");
-//  asm volatile("rdtsc" : "=a" (a), "=d" (d));
-//  return (uint64_t)a | (((uint64_t)d) << 32);
-//}
+static uint64_t perf_test_rdtsc()
+{
+  pro_stopwatch_t stopwatch;
+  uint64_t const iters = 1000000;
+  uint64_t out = 0, tmp;
+  uint64_t diffs = 0;
+  pro_stopwatch_init(&stopwatch);
+  pro_stopwatch_start(&stopwatch);
+  for(uint64_t ii = 0; ii != iters; ++ii) {
+    tmp = my_rdtsc();
+    if (out != 0) {
+      diffs += (tmp - out);
+    }
+    out = tmp;
+    //my_rdtsc();
+    //rdtscll(out);
+    //rdtsc(out, out);
+  }
+  pro_stopwatch_stop(&stopwatch);
+  printf("Average diff: %f\n", ((double)diffs) / iters);
+  return pro_stopwatch_elapsed_nanos(&stopwatch) / iters;
+}
+
+static inline uint64_t my_rdtsc()
+{
+  unsigned a,d;
+  //asm("cpuid");
+  //asm ("rdtsc" : "=a" (a), "=d" (d));
+  asm volatile ("rdtscp" : "=a" (a), "=d" (d));
+  return (uint64_t)a | (((uint64_t)d) << 32);
+  return 0;
+}
 
