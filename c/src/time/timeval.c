5a3dcabf599ed46317b7f94105e6c5eead1e984c
Craig Bowles <staticimport@gmail.com>
2011-09-10 21:49:29 -0500

    Added __APPLE__ specific support in timeval to use gettimeofday since clock_gettime is not available.

--- c/src/time/timeval.c
+++ c/src/time/timeval.c
@@ -1,21 +1,36 @@
 
+#ifndef __APPLE__
 #include <time.h>
+#else
+#include <sys/time.h>
+#endif
 
 #include "timeval.h"
 
 void pro_timeval_now_monotonic(pro_timeval_t* const restrict ts)
 {
+#ifndef __APPLE__
   struct timespec tspec;
   clock_gettime(CLOCK_MONOTONIC_RAW, &tspec);
   ts->seconds = (int32_t)tspec.tv_sec;
   ts->nanoseconds = (int32_t)tspec.tv_nsec;
+#else
+  pro_timeval_now_wall(ts); // NOT GOOD
+#endif
 }
 
 void pro_timeval_now_wall(pro_timeval_t* const restrict ts)
 {
+#ifndef __APPLE__
   struct timespec tspec;
   clock_gettime(CLOCK_REALTIME, &tspec);
   ts->seconds = (int32_t)tspec.tv_sec;
   ts->nanoseconds = (int32_t)tspec.tv_nsec;
+#else
+  struct timeval tv;
+  gettimeofday(&tv, 0);
+  ts->seconds = (int32_t)tv.tv_sec;
+  ts->nanoseconds = (int32_t)tv.tv_usec * 1000;
+#endif
 }
 
