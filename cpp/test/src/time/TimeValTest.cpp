4f789a7e23f20b088f33d894be8669410e5d69c8
Craig Bowles <staticimport@gmail.com>
2011-09-18 00:57:05 -0500

    Added monotonic() and wall() tests to C++ TimeVal

--- cpp/test/src/time/TimeValTest.cpp
+++ cpp/test/src/time/TimeValTest.cpp
@@ -1,4 +1,8 @@
 
+extern "C" {
+#include <sys/time.h>
+}
+
 #include "Testing.hpp"
 #include "TimeVal.hpp"
 #include "TimeValTest.hpp"
@@ -6,6 +10,8 @@
 using namespace promote;
 using namespace promote::testing;
 
+static std::string unitTestMonotonic();
+static std::string unitTestWall();
 static std::string unitTestXseconds(int64_t (TimeVal::*func)() const,
                                     int64_t const div);
 static std::string unitTestSubSeconds(int64_t (TimeVal::*func)() const,
@@ -27,6 +33,8 @@ void promote::testing::perfTestTimeVal()
 
 void promote::testing::unitTestTimeVal()
 {
+  printUnitResult("TimeVal: monotonic()", unitTestMonotonic());
+  printUnitResult("TimeVal: wall()", unitTestWall());
   printUnitResult("TimeVal: microseconds()",
                   unitTestXseconds(&TimeVal::microseconds, 1000));
   printUnitResult("TimeVal: milliseconds()",
@@ -51,6 +59,42 @@ void promote::testing::unitTestTimeVal()
   printUnitResult("TimeVal: >=", unitTestGreaterThanOrEq());
   printUnitResult("TimeVal: +=", unitTestIncrease());
   printUnitResult("TimeVal: -=", unitTestDecrease());
+}
+
+static std::string unitTestMonotonic()
+{
+  // Assert increasing
+  TimeVal prev(0), now(0);
+  for(int ii = 0; ii != 100; ++ii) {
+    now = TimeVal::monotonic();
+    if (now < prev) {
+      return "Monotonic timestamp went backwards!";
+    }
+    prev = now;
+  }
+  return "";
+}
+
+static std::string unitTestWall()
+{
+  for(int ii = 0; ii != 100; ++ii) {
+    struct timeval before, after;
+    gettimeofday(&before, 0);
+    asm("cpuid"); // serialize
+    TimeVal const now(TimeVal::wall());
+    asm("cpuid"); // serialize
+    gettimeofday(&after, 0);
+    if (now.seconds() < before.tv_sec or
+        (now.seconds() == before.tv_sec and now.subSecondMicros() < before.tv_usec))
+    {
+      return "Wall time was less than a previous gettimeofday result.";
+    } else if (after.tv_sec < now.seconds() or
+        (after.tv_sec == now.seconds() and after.tv_usec < now.subSecondMicros()))
+    {
+      return "Wall time was greater than a subsequent gettimeofday result.";
+    }
+  }
+  return "";
 }
 
 static std::string unitTestXseconds(int64_t (TimeVal::*func)() const,
