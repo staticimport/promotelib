9633852a715159eae7d80e7b7731ddc521f935ea
Craig Bowles <staticimport@gmail.com>
2011-09-18 02:11:32 -0500

    Added Padded<T> template that pads an instance of T to reach a size of 1,2,4, or a multiple of 8.

---
+++ cpp/src/common/Padded.hpp
@@ -0,0 +1,65 @@
+#ifndef PROMOTE_PADDED_HPP
+#define PROMOTE_PADDED_HPP
+
+namespace promote {
+  template <typename T>
+  class Padded {
+  public:
+    static std::size_t const PADDING_COUNT;
+
+    Padded();
+    Padded(T const& value);
+    
+    // Const
+    inline T const& value() const;
+
+    // Non-Const
+    inline T& value();
+    Padded<T>& operator=(Padded<T> const& p);
+  private:
+    T _value;
+    char _padding[PADDING_COUNT];
+  };
+}
+
+/** 
+ * Implementation
+ **/
+template <typename T>
+std::size_t const promote::Padded<T>::PADDING_COUNT =
+  sizeof(T) > 4 ? ((sizeof(T) % 8) ? 8 - (sizeof(T) % 8) : 0) : (sizeof(T) == 3 ? 1 : 0);
+
+template <typename T>
+promote::Padded<T>::Padded()
+{
+}
+
+template <typename T>
+promote::Padded<T>::Padded(T const& value)
+: _value(value)
+{
+}
+
+template <typename T>
+inline T const& promote::Padded<T>::value() const
+{
+  return _value;
+}
+
+template <typename T>
+inline T& promote::Padded<T>::value()
+{
+  return _value;
+}
+
+template <typename T>
+promote::Padded<T> const& operator=(promote::Padded<T> const& padded)
+{
+  if (this != &padded) {
+    _value = padded.value();
+  }
+  return *this;
+}
+
+#endif /* PROMOTE_PADDED_HPP */
+
